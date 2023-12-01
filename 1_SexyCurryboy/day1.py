f12b28e1ed40b931f821eadd1a489ceb64cd2aa2
SexyCurryboy <46899915+SexyCurryboy@users.noreply.github.com>
2023-12-01 10:34:54 +0100

    Solved day1.2 (Chli Inspiration isch nötig gsi)

--- 1_SexyCurryboy/day1.py
+++ 1_SexyCurryboy/day1.py
@@ -1,16 +1,28 @@
-import re
 input = []
 with open('input.txt', 'r') as f:
     for line in f:
         input.append(line[:-1])
 
-### Part 1 ###
-digits = []
-values = []
+spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+digits1 = []
+digits2 = []
+values1 = []
+values2 = []
 for line in input:
-    for char in line:
-        if char.isdigit():
-            digits.append(char)
-    values.append(int(digits[0]+digits[-1]))
-    digits = []
-print(sum(values))
+    for i in range(len(line)):
+        #Für Teil 1 & 2
+        if line[i].isdigit():
+            digits1.append(line[i])
+            digits2.append(line[i])
+        #Nur für Teil 2
+        else:
+            for number in spelled:
+                if line[i:(i+len(number))] == number:
+                    digits2.append(str(spelled.index(number)+1))
+
+    values1.append(int(digits1[0] + digits1[-1]))
+    values2.append(int(digits2[0] + digits2[-1]))
+    digits1 = []
+    digits2 = []
+print(sum(values1))
+print(sum(values2))
