a6c06c1ad37d62d802a2291b0b2b5a6a3ea5c33c
SexyCurryboy <46899915+SexyCurryboy@users.noreply.github.com>
2023-12-06 17:34:06 +0100

    Now with Mitternachtsformel for both parts!!! Also added a timer

--- 06_SexyCurryboy/day6.py
+++ 06_SexyCurryboy/day6.py
@@ -1,12 +1,12 @@
 import re
 import math
+import time as ti
 
 with open('input.txt', 'r') as f:
     time = re.findall("[0-9]+", f.readline())
     distance = re.findall("[0-9]+", f.readline())
 new_time = int("".join(time))
 new_distance = int("".join(distance))
-import time as ti
 
 
 def check_for_win(t, d):
@@ -28,6 +28,6 @@ for i in range(len(time)):
     error_margin_1 = check_for_win(int(time[i]), int(distance[i]))[0] * error_margin_1
 runtime_1 = ti.perf_counter() - start_time_1
 
-print(f"The margin for error is {error_margin_1} in part 1. Runtime: {1e+6*runtime_1:.2f}µs")
+print(f"The margin for error is {error_margin_1} in part 1. Runtime: {1e+6 * runtime_1:.2f}µs")
 error_margin_2, runtime_2 = check_for_win(new_time, new_distance)
-print(f"The margin for error is {error_margin_2} in part 2. Runtime: {1e+6*runtime_2:.2f}µs")
+print(f"The margin for error is {error_margin_2} in part 2. Runtime: {1e+6 * runtime_2:.2f}µs")
